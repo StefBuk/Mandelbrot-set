dc4bb5fdcd397de79abd65aeff74d329b64518df
Stefan <74067980+StefBuk@users.noreply.github.com>
2024-03-07 23:24:04 +0100

    Update fun.h
    Changing name of variable

--- fun.h
+++ fun.h
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <fstream>
 
-std::fstream file_opener( const char *name, int nop )
+std::fstream file_opener( const char *name, int how )
 {
   std::fstream str;
 
-  if( nop == 1 )   str.open( name, std::ofstream::in | std::fstream::out | std::fstream::trunc );
-  if( nop == 2 )   str.open( name, std::ofstream::app );
+  if( how == 1 )   str.open( name, std::ofstream::in | std::fstream::out | std::fstream::trunc );
+  if( how == 2 )   str.open( name, std::ofstream::app );
 
   if( !str.is_open() )
   {
-    std::cout << "Greska pri otvaranju fajla!";
+    std::cout << "File opening error!";
     exit( EXIT_FAILURE );
   }
 
